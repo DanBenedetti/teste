b8858fad79a0e29157cee15d6af6ea7c12070e7c
Danilo Benedetti Ribeiro <danilobenedetti1@gmail.com>
2025-05-21 20:44:08 -0300

    atualização da configuração de conexão

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host:"localhost",
     user:"fukuta",
     password:"FatecFranca123#",
-    database: "MINHA_API"
+    database: "danilopi"
 });
 
 module.exports = connection;
