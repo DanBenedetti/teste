93cc9157c327377b0cbcaed9ce85fa2778207769
DanBenedetti <danilobenedetti1@gmail.com>
2025-05-23 10:44:24 -0300

    ajustando o controller

--- home/homeController.js
+++ home/homeController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const connection = require("./database/connection");
+const connection = require("../database/connection");
 
 router.get("/", (req, res) => {
     res.render("index");
